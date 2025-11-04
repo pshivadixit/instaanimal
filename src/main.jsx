0422e3317603c08b184204085ebed11cc54b2db4
pshivadixit <pshivadixit@gmail.com>
2025-11-04 20:35:11 +0530

    Fix GitHub Pages 404 issue (added base path and redirect)

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,10 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './styles/styles.css'
 
-
 createRoot(document.getElementById('root')).render(
-<React.StrictMode>
-<BrowserRouter>
-<App />
-</BrowserRouter>
-</React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter basename="/instaanimal">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 )
