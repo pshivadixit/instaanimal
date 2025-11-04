021e4152b1e5fb7e8be9a79b5e991cc16c4c6d6c
pshivadixit <pshivadixit@gmail.com>
2025-11-04 19:53:14 +0530

    Fix GitHub Pages 404 issue (added base path and redirect)

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/insta-animal/', // 👈 important for GitHub Pages
   plugins: [
     react({
       babel: {
